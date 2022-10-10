128bd0042892db77b44c4fd1ff434077afab08a1
Tapotix <gaulmatthieu@gmail.com>
2022-10-10 15:43:22 +0200

    Auto stash before merge of "main" and "origin/main"

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 
 import metier.Jeu;
+<<<<<<< Updated upstream
 import model.Matiere;
 
 import java.util.ArrayList;
@@ -8,24 +9,28 @@ import java.util.Collections;
 import java.util.List;
 
 import metier.Achat;
+=======
+import metier.Portable;
+import metier.Salon;
+>>>>>>> Stashed changes
 import metier.Client;
-import metier.Console;
+import metier.Hybride;
 
 public class Test {
 
 	public static void main(String[] args) {
 
-		Console c1 = new Console("PC");
-		Console c2 = new Console("XBOX360");
-		Console c3 = new Console("Wii");
-		Console c4 = new Console("PS1");
-		Console c5 = new Console("Gamecube");
+		Salon c1 = new Salon("PC");
+		Hybride c2 = new Hybride("Switch");
+		Portable c3 = new Portable("DS");
+		Salon c4 = new Salon("PS1");
+		Salon c5 = new Salon("Gamecube");
 
-		Jeu j1 = new Jeu("WoW", c1);
-		Jeu j2 = new Jeu("Fallout New Vegas", c2);
-		Jeu j3 = new Jeu("MarioKart", c3);
-		Jeu j4 = new Jeu("Final Fantasy 7", c4);
-		Jeu j5 = new Jeu("Super Mario Sunshine", c5);
+		Jeu j1 = new Jeu("WoW", c1, null);
+		Jeu j2 = new Jeu("Fallout New Vegas", c2, null);
+		Jeu j3 = new Jeu("MarioKart", c3, null);
+		Jeu j4 = new Jeu("Final Fantasy 7", c4, null);
+		Jeu j5 = new Jeu("Super Mario Sunshine", c5, null);
 		
 		Achat a1 = new Achat(j1,"2022-10-10",50);
 		Achat a2 = new Achat(j2,"2022-10-15",60);
