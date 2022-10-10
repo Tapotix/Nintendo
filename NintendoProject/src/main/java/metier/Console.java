7366fd94aa24d5d5d421f5c95cdf3003707388eb
Mohamed ALSIBAI <msibai2@gmail.com>
2022-10-10 15:18:25 +0200

    client

--- NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/src/main/java/metier/Console.java
@@ -3,23 +3,64 @@ package metier;
 public class Console {
 
 	private String nom;
+	private double prix;
+	private String dateSortie;
 
-	public Console(String nom) {
+
+
+	public Console(String nom, double prix, String dateSortie) {
+		super();
 		this.nom = nom;
-	}
+		this.prix = prix;
+		this.dateSortie = dateSortie;
+	
+	
+	
+}
+
+
 
 	public String getNom() {
 		return nom;
 	}
 
+
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+
+
+	public double getPrix() {
+		return prix;
+	}
+
+
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+
+
+	public String getDateSortie() {
+		return dateSortie;
+	}
+
+
+
+	public void setDateSortie(String dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", dateSortie=" + dateSortie + ", getNom()=" + getNom()
+				+ ", getPrix()=" + getPrix() + ", getDateSortie()=" + getDateSortie() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
-	
 	
 }
